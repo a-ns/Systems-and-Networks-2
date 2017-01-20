5743598c2a2381a68ef654cef66ba88d2b2e450c
AleksanderS <exmann94@gmail.com>
2017-01-19 19:56:53 -0600

    Fixing Windows CRLF bad encoding

--- Project1/bbserver.c
+++ Project1/bbserver.c
@@ -1,1 +1,1 @@
-/** @filename bbserver.c* @author Alex Lindemann , Nathan Moore* @created 1/19/2017** @desc This is a server program for a token ring simulation.*/int main (int argc, char *argv[]) {  return;}
+/** @filename bbserver.c* @author Alex Lindemann , Nathan Moore* @created 1/19/2017** @desc This is a server program for a token ring simulation.*/int main (int argc, char *argv[]) {  return 0;}
