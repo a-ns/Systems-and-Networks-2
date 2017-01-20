53c58597d8d062c5f01c97428c7cf40e892cfc6b
AleksanderS <exmann94@gmail.com>
2017-01-19 20:01:32 -0600

    Fixing carriage returns. Lets try one more time

--- Project1/bbserver.c
+++ Project1/bbserver.c
@@ -1,1 +1,12 @@
-/** @filename bbserver.c* @author Alex Lindemann , Nathan Moore* @created 1/19/2017** @desc This is a server program for a token ring simulation.*/int main (int argc, char *argv[]) {  return 0;}
+/*
+* @filename bbserver.c
+* @author Alex Lindemann , Nathan Moore
+* @created 1/19/2017
+*
+* @desc This is a server program for a token ring simulation.
+*/
+
+int main (int argc, char *argv[]) {
+
+  return 0;
+}
