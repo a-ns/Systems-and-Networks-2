35d88b93ed5eb9977e9335fd9341bd22955e21d3
a-ns <exmann94@gmail.com>
2017-03-12 20:13:29 -0500

    Started on sendMessage(..) in sender.c

--- Project2/sender.c
+++ Project2/sender.c
@@ -17,18 +17,14 @@
 
 void getMessageFromUser(char *);
 void checkCommandLineArguments(int, char**);
-void setup_network (char **, int *, struct hostent**, struct sockaddr_in *, struct sockaddr_in *, int *, int *, int *);
+int setup_network (int* , char *, int , char *, int , struct hostent** , struct sockaddr_in * , struct sockaddr_in * , int);
 int main (int argc, char *argv[]) {
   checkCommandLineArguments(argc, argv);
   char messageToSend[500];
   getMessageFromUser(messageToSend);
-  int sockfd;
-  struct hostent *hostptr;
-  struct sockaddr_in network, src;
-  int ME_PORT;
-  int DEST_PORT;
-  int NETWORK_PORT;
-  setup_network(argv, &sockfd, &hostptr, &network, &src, &ME_PORT, &DEST_PORT, &NETWORK_PORT);
+  sendMessage(atoi(argv[1]), argv[4], atoi(argv[5]), argv[2], atoi(argv[3]), messageToSend);
+
+  //setup_network(argv, &sockfd, &hostptr, &network, &src, &ME_PORT, &DEST_PORT, &NETWORK_PORT);
 
   return 0;
 }
@@ -40,33 +36,29 @@ void checkCommandLineArguments(int argc, char **argv) {
   }
 }
 
-void setup_network(char **argv, int *sockfd, struct hostent**hostptr, struct sockaddr_in * network, struct sockaddr_in *src, int *ME_PORT, int *DEST_PORT, int *NETWORK_PORT) {
-  *ME_PORT = atoi(argv[1]);
-  *DEST_PORT = atoi(argv[3]);
-  *NETWORK_PORT = atoi(argv[5]);
+int setup_network(int* sockfd, char *netwhost, int netwPort, char *desthost, int destPort, struct hostent** hostptr, struct sockaddr_in * network, struct sockaddr_in * src, int localPort){
+*sockfd = socket(AF_INET, SOCK_DGRAM, 0);
+ if (sockfd < 0 ) {
+   perror("error opening socket");
+   return -2;
+ }
+ *hostptr = gethostbyname(netwhost);
+ memset((void *) network, 0, (size_t) sizeof(*network));
+ network->sin_family = AF_INET;
+ memcpy((void *) &(network->sin_addr), (void *) (*hostptr)->h_addr, (*hostptr)->h_length);
+ network->sin_port = htons((u_short) netwPort);
 
-  *sockfd = socket(AF_INET, SOCK_DGRAM, 0);
-  if (sockfd < 0 ) {
-    perror("error opening socket");
-    exit(1);
-  }
-  *hostptr = gethostbyname(argv[4]);
-  memset((void *) network, 0, (size_t) sizeof(*network));
-  network->sin_family = AF_INET;
-  memcpy((void *) &(network->sin_addr), (void *) (*hostptr)->h_addr, (*hostptr)->h_length);
-  network->sin_port = htons((u_short) *NETWORK_PORT);
-
-  memset((void *) src, 0, sizeof(*src));
-  src->sin_family = AF_INET;
-  src->sin_addr.s_addr = htonl(INADDR_ANY);
-  src->sin_port = htons(*ME_PORT);
-  if(bind (*sockfd, (struct sockaddr *) src, sizeof(*src)) < 0) {
-    perror("bind");
-    exit(1);
-  }
-  fprintf(stderr, "\nWe are listening on: %s:%d\n", inet_ntoa(src->sin_addr), ntohs(src->sin_port));
-  fprintf(stderr, "\nWe are sending to the network: %s:%d\n", inet_ntoa(network->sin_addr), ntohs(network->sin_port));
-  return;
+ memset((void *) src, 0, sizeof(*src));
+ src->sin_family = AF_INET;
+ src->sin_addr.s_addr = htonl(INADDR_ANY);
+ src->sin_port = htons(localPort);
+ if(bind (*sockfd, (struct sockaddr *) src, sizeof(*src)) < 0) {
+   perror("bind");
+   return -1;
+ }
+ fprintf(stderr, "\nWe are listening on: %s:%d\n", inet_ntoa(src->sin_addr), ntohs(src->sin_port));
+ fprintf(stderr, "\nWe are sending to the network: %s:%d\n", inet_ntoa(network->sin_addr), ntohs(network->sin_port));
+return 0;
 }
 
 /*
@@ -77,4 +69,15 @@ void getMessageFromUser(char *buffer) {
   printf("Enter the message to send: ");
   fgets(buffer, 500, stdin);
   return;
+}
+
+int sendMessage (int localPort, char* netwhost, int netwPort, char* desthost, int destPort, char* message) {
+  int sockfd;
+  struct hostent *hostptr;
+  struct sockaddr_in network, src;
+  setup_network(&sockfd, netwhost, netwPort, desthost, destPort, &hostptr, &network, &src, localPort);
+  
+
+
+  return 0;
 }
